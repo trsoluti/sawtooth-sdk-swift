a1df6d4eef31426586107eb5cc8be694e841fae9
Teaching <thomas_odell@rp.edu.sg>
2019-08-18 21:03:46 +0800

    Added a dummy ZeroMQ framework. Doesn't seem to be a way to make it multi-platform.

---
+++ Sources/ZeroMQSwift/ZeroMQSwift.h
@@ -0,0 +1,19 @@
+//
+//  ZeroMQSwift.h
+//  ZeroMQSwift
+//
+//  Created by Teaching on 18/8/19.
+//  Copyright © 2019 Bitwise IO. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for ZeroMQSwift.
+FOUNDATION_EXPORT double ZeroMQSwiftVersionNumber;
+
+//! Project version string for ZeroMQSwift.
+FOUNDATION_EXPORT const unsigned char ZeroMQSwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZeroMQSwift/PublicHeader.h>
+
+
---
+++ Tests/ZeroMQSwiftTests/ZeroMQSwiftTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZeroMQSwiftTests.swift
+//  ZeroMQSwiftTests
+//
+//  Created by Teaching on 18/8/19.
+//  Copyright © 2019 Bitwise IO. All rights reserved.
+//
+
+import XCTest
+@testable import ZeroMQSwift
+
+class ZeroMQSwiftTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
